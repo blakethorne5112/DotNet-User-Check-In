309e7bebb0a187d6ebe939452ba8e175b212afad
Josh Wells <wellsy9722@gmail.com>
2023-10-27 00:44:27 +1100

    Added column for permissionlevel to AspnetUsers table

---
+++ DotNetAssign2/Models/ManagerUsers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DotNetAssign2.Models
+{
+    public class ManagerUsers : IdentityUser
+    {
+        public PermissionLevel Role { get; set; }
+
+        public enum PermissionLevel
+        {
+            Guest,
+            User,
+            Moderator,
+            Admin,
+        }
+
+    }
+}
