1b3d65052edccfca601e1b106eeb01896b97dcd2
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-10-28 23:56:26 +1100

    most work done, db pain sorted

--- DotNetAssign2/Controllers/LocationsController.cs
+++ DotNetAssign2/Controllers/LocationsController.cs
@@ -3,6 +3,7 @@ using DotNetAssign2.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace DotNetAssign2.Controllers
 {
